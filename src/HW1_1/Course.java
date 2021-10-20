117e89c68798ce81f52c711baac48fd380ae5932
Lisin Ilya <i.baudex@mail.ru>
2021-10-20 23:03:53 +0300

    Исправление первого ДЗ

--- src/HW1_1/Course.java
+++ src/HW1_1/Course.java
@@ -1,4 +1,22 @@
 package HW1_1;
 
 public class Course {
+
+    int[] obstacles;
+
+    public Course(int run, int jump, int swim){
+        obstacles = new int[]{run, jump,swim};
+    }
+
+    public void finishCourse(Team team){
+        for(Member member: team.members){
+            for(int obstacle:obstacles){
+                if(member.runLength >= obstacle && member.jumpLength >= obstacle && member.swimLength >= obstacle){
+                    System.out.println(member.name + " finished");
+                }
+            }
+        }
+    }
+
+
 }
--- src/HW1_1/Team.java
+++ src/HW1_1/Team.java
@@ -1,6 +1,16 @@
 package HW1_1;
 
 public class Team {
-    Team
+    String name;
+    Member[]members;
 
+    public Team(String name, Member member1, Member member2, Member member3, Member member4){
+        members = new Member[]{member1, member2, member3, member4};
+        this.name = name;
+    }
+    public void printInfoAboutMembers(){
+        for(Member member:members){
+            member.info();
+        }
+    }
 }
